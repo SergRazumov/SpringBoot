d16fe8212cd817eed0707b17d0aeadf9ecceecbf
razumov <razumov@protei.ru>
2023-04-27 10:39:25 +0300

    Squashed

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
+    SpringApplication.run(Main.class);
+    System.out.println("Это ветка Sergey");
+    System.out.println("Commit1 from Sergey для переноса");
+    System.out.println("Commit2 from Sergey для переноса");
   }
 
 }
