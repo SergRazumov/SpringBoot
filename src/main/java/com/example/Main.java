a8cd6d7d7c83cecb0e4d2707096ad42bbe40bea8
razumov <razumov@protei.ru>
2023-05-05 11:58:00 +0300

    Коммит

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,8 +8,11 @@ public class Main {
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class);
-    System.out.println("Test");
     System.out.println("Это ветка Sergey");
+    System.out.println("Commit1 from Sergey для переноса");
+    System.out.println("Commit2 from Sergey для переноса");
+    System.out.println();
+
   }
 
 }
